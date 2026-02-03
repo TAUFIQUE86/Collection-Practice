8fcb13bc227627f9cbac53071c28be56f2a33179
Vivekananda <vivekanandadash245@gmail.com>
2026-02-03 13:07:17 +0530

    Add new

--- src/tesgt/MyClass.java
+++ src/tesgt/MyClass.java
@@ -6,5 +6,8 @@ public class MyClass {
     public static void main(String[] args) {
         ArrayDeque<Integer> arr = new ArrayDeque<>();
         arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        System.out.println(arr);
     }
 }
